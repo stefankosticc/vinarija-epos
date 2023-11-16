2350fd07afc95568800da38482db57bc3e1b8d0c
stefankosticc <skostic806@gmail.com>
2023-11-16 15:06:11 +0100

    Sve zivo

--- Footer/footer.js
+++
@@ -1,9 +0,0 @@
-function createFooter() {
-  const footer = document.createElement("footer");
-  footer.classList.add("footer-container");
-  const p = document.createElement("p");
-  p.textContent = "Vinarija Joksimovic";
-  p.classList.add("footer-ime");
-  footer.appendChild(p);
-  return footer;
-}
